f00007475f64e2c7adaf7209d5725d6f3d5d6629
maouia <maouiabend@gmail.com>
2024-04-14 18:37:11 +0100

    style

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" Flip Me Over """
+""" line up """
 
 
 def add_arrays(arr1, arr2):
-    """ transpose a 2D matrix """
-    if (len(arr1)!= len(arr2)):
+    """ adds two arrays element-wise """
+    if (len(arr1) != len(arr2)):
         return None
     return [arr1[i] + arr2[i] for i in range(len(arr1))]
