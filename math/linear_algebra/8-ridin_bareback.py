4dac2ab71108ea820f3463c44b20bf12b846d772
maouia <maouiabend@gmail.com>
2024-04-16 00:09:55 +0100

    Ridin’ Bareback

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Ridin’ Bareback"""
+
+
+def mat_mul(mat1, mat2):
+    """ matrix multiplication """
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    new_matrix = [[0] * len(mat2[0]) for _ in range(len(mat1))]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                new_matrix[i][j] += mat1[i][k] * mat2[k][j]
+    return new_matrix
