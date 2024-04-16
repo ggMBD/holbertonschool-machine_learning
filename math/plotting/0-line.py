aba9a06ea22f2ecfcb8ae54bed2194c75bc57373
maouia <maouiabend@gmail.com>
2024-04-16 17:19:51 +0100

    doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+""" 0. Line Plot """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """ Line Plot"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
