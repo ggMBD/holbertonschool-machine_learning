56ba660749babf5c73d03a7b0fc588dbc5a71f65
maouia <maouiabend@gmail.com>
2024-06-06 10:40:00 -0400

    Optimize

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Optimize Optimize"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """sets up Adam optimization for a
+    keras model with categorical crossentropy
+    loss and accuracy metricssets up Adam optimization
+    for a keras model with categorical
+    crossentropy loss and accuracy metrics"""
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2)
+    network.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
